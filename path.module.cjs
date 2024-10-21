6b031201a3bc5fa274b1b709a288ee616239231e
Cristian Racedo <cristiandracedo@hotmail.com>
2024-10-21 16:50:08 -0300

    agregar ejemplos con modulo path

---
+++ path.module.cjs
@@ -0,0 +1,18 @@
+const path = require('node:path');
+/** VARIABLES GLOBALES COMMONJS
+ * __dirname: Ruta de la carpeta que ejecuta el script
+ * __filename: Ruta del archivo que se esta ejecutando
+ */
+
+// Extrae las partes de la ruta (Descomposicion)
+console.log("Identificacion de partes: ", path.parse(__filename)) 
+// Genera una ruta a partir de los argumentos (Composicion)
+console.log("Generacion de Ruta: ", path.resolve("index.html"))
+
+function extract(file){
+    return path.parse(file);
+}
+function create(file){
+    return path.resolve(file);
+}
+module.exports { extract, create }
