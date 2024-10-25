0e9e1633185d2daeb0a24d3a0501e3210178dfd3
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-10-25 14:59:15 -0300

    servir archivos estaticos con fs

--- index.js
+++ index.js
@@ -1,53 +1,42 @@
+import { readFile } from "fs";
 import { createServer } from "http";
 
 const { PORT=3000 } = process.env;
 const app = createServer( (req,res) => {
     let type = "text"
-    let data = "Bienvenido al Servidor"
+    let data = "public/404.html"
 
     switch(req.method){
         case "GET":
-            if (/\/|index|\.html|home/.test(req.url)){
+            if (/^\/$|\.html|home/.test(req.url)){
                 type = "text/html";
-                data = `
-                    <h1>Fundacion Pescar</h1>
-                    <p>Capacitacion NodeJS</p>`;
+                data = 'public/index.html';
             }
-            if (/styles|default\.css|\.css|css/.test(req.url)){
+            if (/styles|default\.css|\.css/.test(req.url)){
                 type = "text/css";
-                data = `
-                    * {
-                        box-sizing: border-box;
-                        font-family: sans-serif;
-                    }
-                    body{
-                        margin: 0;
-                    }`
+                data = 'public/styles/default.css'
             }
             if (/index\.js|script|main\.js|\.js/.test(req.url)){
                 type = "text/javascript";
-                data =" document.addEventListener('DOMContentLoaded', () => { console.log(\"Contenido Cargado\");})"
+                data = 'public/scripts/index.js'
             }
             if (/\.json|request|api$/.test(req.url)){
                 type = "application/json";
-                data = JSON.stringify({
-                    title: "Fundacion Pescar",
-                    message: "Bienvenido al Servidor"
-                })
+                data = 'public/assets/data.json'
             }
         break;
         case "POST":
-            data = "Datos recibidos"
         break;
         case "PUT":
-            data = "Actualizacion Solicitada"
         break;
         case "DELETE":
-            data = "Eliminacion Procesada"
         break;
     }
-    res.writeHead(200, { "Content-Type": type})
-    res.write(data)
-    res.end()
+    readFile(data, (err, data) => {
+        if (err) throw err
+        console.log(req.url)
+        res.writeHead(200, { "Content-Type": type})
+        res.end(data)
+    })
 })
 app.listen(PORT, () => console.log("Servicio ejecutandose"))
