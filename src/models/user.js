1f832b8603b12ddc788e5c2bca06f2c231cd15cf
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-10-28 16:49:18 -0300

    definir modelo de datos de usuarios

---
+++ src/models/user.js
@@ -0,0 +1,20 @@
+const usersModel = [
+    {
+        id: 1,
+        user: "cristian",
+        email: "cristiandracedo@hotmail.com"
+    },
+    
+    {
+        id: 2,
+        user: "c215714n",
+        email: "cristiandracedo@gmail.com"
+    },
+    
+    {
+        id: 3,
+        user: "d477714n",
+        email: "cristiandracedo@ymail.com"
+    }
+];
+export default usersModel;
