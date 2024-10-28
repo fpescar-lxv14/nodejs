32845df0ac28d905f160a6a729d20be0f309743c
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-10-28 16:51:08 -0300

    implementar mrc de usuarios

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+// Dependencias del Proyecto
 import express from "express";
-
-const { PORT=3000 } = process.env;
-
+// Dependencias de Desarrollo
+import { configDotenv } from "dotenv";
+// Importacion de Modulos
+import usersRouter from "./src/routes/user.js";
+// Configuracion
+configDotenv();
+// Declaraciones
+const { 
+    APP="Mi Aplicacion", 
+    DESCRIPTION="inserte descripcion aqui",
+    VERSION:version="v0.0.0-beta",
+    PORT=3000
+} = process.env;
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+// Implementacion de Rutas
+app.use("/users", usersRouter);
 app.use(express.static('public'));
 
 app.listen(PORT, () => console.log("Servicio ejecutandose"))
