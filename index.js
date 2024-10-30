4c417fb7bfddd4a6e8238b01654f5ca667292110
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-10-30 15:39:36 -0300

    instalar y definir motor de plantillas

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Dependencias del Proyecto
+import { resolve } from "path"
 import express from "express";
+import { engine } from "express-handlebars";
 // Dependencias de Desarrollo
 import { configDotenv } from "dotenv";
 // Importacion de Modulos
@@ -14,6 +16,13 @@ const {
     PORT=3000
 } = process.env;
 const app = express();
+app.engine("hbs", engine({
+    layoutsDir: resolve("views/layouts"),
+    partialsDir: resolve("views/partials"),
+    defaultLayout: resolve("views/index.hbs"),
+    extname: "hbs"
+}))
+app.set("view engine", "hbs");
 app.use(express.json()); // Solicitudes FormData
 app.use(express.urlencoded({ extended: false })) // Solicitudes x-www-form-urlencoded
 // Implementacion de Rutas
