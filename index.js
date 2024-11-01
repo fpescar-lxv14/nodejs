0f81378ef800398e9c5f288383db3d7e234b40d9
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-11-01 14:59:55 -0300

    implementar rutas con motor de plantillas

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ import { engine } from "express-handlebars";
 import { configDotenv } from "dotenv";
 // Importacion de Modulos
 import usersRouter from "./src/routes/user.js";
+import appLinks from "./src/data/app.json" with { "type": "json" }
 // Configuracion
 configDotenv();
 // Declaraciones
 const { 
     APP="Mi Aplicacion", 
     DESCRIPTION="inserte descripcion aqui",
-    VERSION:version="v0.0.0-beta",
+    VERSION="v0.0.0-beta",
     PORT=3000
 } = process.env;
 const app = express();
@@ -27,6 +28,18 @@ app.use(express.json()); // Solicitudes FormData
 app.use(express.urlencoded({ extended: false })) // Solicitudes x-www-form-urlencoded
 // Implementacion de Rutas
 app.use("/users", usersRouter);
+app.get("/home", (_, res) => {
+    res.render('index', {
+        title: APP, ...appLinks,
+        description: `${DESCRIPTION} (version: ${VERSION})`,
+    })
+})
+app.get("*", (_,res) => res.render("index", {
+    title: APP, ...appLinks,
+    error: {
+        title: "ERROR 404",
+        message: "Página no encontrada"
+    },
+}))
 app.use(express.static('public'));
-
 app.listen(PORT, () => console.log("Servicio ejecutandose"))
