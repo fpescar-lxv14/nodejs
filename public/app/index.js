77640ee86ada1fe114bbacd675b37bb8128d47f9
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-11-08 16:29:54 -0300

    corregir identificacion de usuario y agregar evento de  carga

--- public/app/index.js
+++ public/app/index.js
@@ -1,19 +1,34 @@
+document.addEventListener("DOMContentLoaded", () => {
 const d = document;
 const socket = io();
 const chatRoom = document.getElementById('chat-room');
 const chatForm = document.getElementById('chat-form');
 
+// Recepcion de Mensaje
 socket.on("message", ({username:u, message:m}) => {
+    const current = chatForm.username.value
     const msg = Object.assign(d.createElement('article'), {
-        className: chatForm.username.value === username ? "self" : "another",
+        className: current === u ? "self" : "another",
         innerHTML: `<h2>${u}</h2><p>${m}</p>`
     })
     chatRoom.appendChild(msg)
 })
-
+// Envio de mensaje
 chatForm.addEventListener('submit', (e) => {
     e.preventDefault()
+    const { username, message } = e.target
     socket.emit("message",{
-        username:"usuario",message:"algun texto, escrito por alguien"
+        username: username.value,
+        message: message.value,
     })
+    message.value = "";
+    message.focus()
+})
+chatForm.message.addEventListener('keyup', (e) => {
+    if (!e.ctrlKey && e.code === "Enter"){
+        chatForm.querySelector('button').click()
+    }
+})
+// Carga de mensajes previos
+    socket.emit("load")
 })
