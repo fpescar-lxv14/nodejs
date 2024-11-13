66e703703afbda7e30576439718fe663a1f52fc9
Cristian Racedo <CristianDRacedo@hotmail.com>
2024-11-13 16:05:59 -0300

    actualizar funcion y pruebas de division

--- src/div.js
+++ src/div.js
@@ -1,3 +1,7 @@
 export function div(a,b){
-
+    const [A,B] = [Number(a), Number(b)];
+    const message = "operacion no permitida"
+    if (isNaN(A) || isNaN(B)) return ({ message, code: 405 })
+    if (!B) return ({ message, code: 0 })
+    return A / B
 }
--- tests/div.test.js
+++ tests/div.test.js
@@ -1,34 +1,39 @@
 import { expect } from "chai"
 import { div } from "../src/div.js"
 // Descripcion General
-let result;
 describe("division de 10 entre 5", () => {
-    result = div(10,5);
-    it("el resultado debe ser 2", () => {
-        expect(result).to.equal(2, "error: 10/5 = 2")
-    })
-    it("el resultado debe ser un numero", () => {
+    const result = div(10,5);
+    it("el resultado debe ser 2", () => 
+        expect(result).to.equal(2,"error: 10/5 = 2")
+    )
+    it("el resultado debe ser un numero", () => 
         expect(result).to.be.a("number", "solo se admite el tipo Number")
-    })
+    )
 })
 describe("division de 3 entre false", () => {
-    result = div(3,false);
-    it("debe dar un mensaje de error", () => {
+    const result = div(3,false);
+    it("debe dar un mensaje de error", () => 
         expect(result).to.be.an('object')
-    })
-    it("debe devolver un codigo de error", () => {
+    )
+    it("debe devolver un codigo de error", () => 
         expect(result).to.have.property('code', 0)
-    })
-    it("debe identificar el problema", () => {
+    )
+    it("debe identificar el problema", () => 
         expect(result).to.have.property("message", "operacion no permitida")
-    })
+    )
 })
 describe("division de 0 entre '25'", () => {
-    result = div(0,"25")
-    it("debe dar como resultado 0", () => {
+    const result = div(0,"25")
+    it("debe dar como resultado 0", () => 
         expect(result).to.equal(0)
-    })
-    it("debe ser de tipo numerico", () => {
+    )
+    it("debe ser de tipo numerico", () => 
         expect(result).to.be.a('number')
+    )
+})
+describe("division de [] entre {} ", () => {
+    const result = div([],{})
+    it("deber mostrar el codigo 405", () => {
+        expect(result).to.have.property("code",405)
     })
 })
